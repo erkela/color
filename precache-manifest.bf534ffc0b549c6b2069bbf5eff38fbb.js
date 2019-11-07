fbfa677cfd01a6dac57ccfe7748da8575a0ed028
Bartusiak <odin.bartusiak@gmail.com>
2019-11-07 17:23:24 +0100

    Updates

---
+++ precache-manifest.bf534ffc0b549c6b2069bbf5eff38fbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49579e5a1533d9d874e9d0ffed4e7bbc",
+    "url": "/erkela/color/index.html"
+  },
+  {
+    "revision": "e227fd58049eb945d953",
+    "url": "/erkela/color/static/css/main.33b92f94.chunk.css"
+  },
+  {
+    "revision": "ad42e636797b6acb62a7",
+    "url": "/erkela/color/static/js/2.e7b653b2.chunk.js"
+  },
+  {
+    "revision": "e227fd58049eb945d953",
+    "url": "/erkela/color/static/js/main.7c19e0d1.chunk.js"
+  },
+  {
+    "revision": "cfd5da91e84f90b3449e",
+    "url": "/erkela/color/static/js/runtime-main.c64de110.js"
+  }
+]);
